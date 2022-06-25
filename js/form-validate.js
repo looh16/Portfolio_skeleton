c76c5a81209d01d6f623d637f13782f3f3d87229
Custodio Serafim Langa <langacustodio@gmail.com>
2022-06-25 14:34:44 +0200

    Add code review recommendations

--- js/form-validate.js
+++ js/form-validate.js
@@ -1,34 +1,7 @@
 const email = document.querySelector('input[name=user_email]');
-const button = document.querySelector('#btn-get-touch');
-const text = document.querySelector('#message');
 
-const validateEmail = (email) => {
-  /* eslint-disable no-useless-escape */
-  const regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return regex.test(String(email).toLowerCase());
-};
-
-const validateLowerCaseEmail = (email) => {
-  /* eslint-disable no-useless-escape */
-  const regex = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
-  return regex.test(email);
-};
-
-button.addEventListener('click', (event) => {
-  event.preventDefault();
-
-  if (validateLowerCaseEmail(email.value)) {
-    return true;
-    text.innerText = '';
-  } else {
-    text.innerText = 'Please the email must be in lowercase';
-    return false;
-  }
-
-  if (!validateEmail(email.value)) {
-    text.innerText = 'Please provide a valid email address';
-    return false;
-  }
-
-  email.value = " ";
-});
+function lowerCase() {
+  email.value = email.value.toLowerCase();
+}
+email.addEventListener('keyup', lowerCase);
+document.addEventListener('click', formSubmit);
