4acc10d58e2cf1c80aabd0849c3f58345d970427
Custodio Serafim Langa <langacustodio@gmail.com>
2022-06-25 19:10:45 +0200

    solve eslint errors

--- js/form-validate.js
+++ js/form-validate.js
@@ -1,6 +1,6 @@
-let userName = document.querySelector("#name")
-let userEmail = document.querySelector("#mail")
-let userMessage = document.querySelector("#msg")
+const userName = document.querySelector('#name');
+const userEmail = document.querySelector('#mail');
+const userMessage = document.querySelector('#msg');
 
 const formFields = {
   userName: userName.value,
@@ -8,36 +8,29 @@ const formFields = {
   userMessage: userMessage.value,
 };
 
-
-userName.onkeyup = function() {getNameData()};
-userEmail.onkeyup = function() {getMailData(), lowerCase()};
-userMessage.onkeyup = function() {getMessageData()};
-
-
 function getNameData() {
   formFields.userName = userName.value;
-  localStorage.setItem("name", formFields.userName)
+  localStorage.setItem('name', formFields.userName);
 }
 
 function getMailData() {
   formFields.userEmail = userEmail.value;
-  localStorage.setItem("mail", formFields.userEmail)
+  localStorage.setItem('mail', formFields.userEmail);
 }
 
 function getMessageData() {
   formFields.userMessage = userMessage.value;
-  localStorage.setItem("message", formFields.userMessage)
+  localStorage.setItem('message', formFields.userMessage);
 }
 
 function lowerCase() {
   userEmail.value = userEmail.value.toLowerCase();
 }
 
-userName.value = localStorage.getItem("name");
-userEmail.value = localStorage.getItem("mail");
-userMessage.value = localStorage.getItem("message");
+userName.onkeyup = function () { getNameData(); };
+userEmail.onkeyup = function () { getMailData(); lowerCase(); };
+userMessage.onkeyup = function () { getMessageData(); };
 
-email.addEventListener('keyup', lowerCase);
-document.addEventListener('click', formSubmit);
-
-document.addEventListener('click', storeDataInLocalStorage);
+userName.value = localStorage.getItem('name');
+userEmail.value = localStorage.getItem('mail');
+userMessage.value = localStorage.getItem('message');
